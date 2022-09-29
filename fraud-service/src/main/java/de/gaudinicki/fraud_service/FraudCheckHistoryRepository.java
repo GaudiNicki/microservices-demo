b9d09fd4b926529038d7ac2c52a2885d095165ba
Niklas Witzel <witzel.niklas@gmx.de>
2022-09-29 12:49:00 +0200

    implement basic fraud functionality

---
+++ fraud-service/src/main/java/de/gaudinicki/fraud_service/FraudCheckHistoryRepository.java
@@ -0,0 +1,9 @@
+package de.gaudinicki.fraud_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
+
+}
---
+++ fraud-service/src/main/java/de/gaudinicki/fraud_service/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package de.gaudinicki.fraud_service;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
