bdfa52eb71d3f8aac9251d6ec556ffdbfa42dfc2
Niklas Witzel <witzel.niklas@gmx.de>
2022-09-29 12:48:36 +0200

    transform records back to classes in customer-service

---
+++ customer-service/src/main/java/de/gaudinicki/customer_service/CustomerConfig.java
@@ -0,0 +1,8 @@
+package de.gaudinicki.customer_service;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerConfig {
+
+}
--- customer-service/src/main/java/de/gaudinicki/customer_service/CustomerService.java
+++ customer-service/src/main/java/de/gaudinicki/customer_service/CustomerService.java
@@ -1,19 +1,25 @@
 package de.gaudinicki.customer_service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
-                .firstName(customerRegistrationRequest.firstName())
-                .lastName(customerRegistrationRequest.lastName())
-                .email(customerRegistrationRequest.email())
-                .build();
+            .firstName(customerRegistrationRequest.firstName())
+            .lastName(customerRegistrationRequest.lastName())
+            .email(customerRegistrationRequest.email())
+            .build();
 
         // TODO: check if email is valid
         // TODO: check if email is not already taken
-        // TODO: store customer in DB
+        // TODO: check if fraudster
         customerRepository.save(customer);
+        // TODO: send notification
     }
 }
